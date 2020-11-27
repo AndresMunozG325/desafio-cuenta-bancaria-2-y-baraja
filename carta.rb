0466e60c30fff3fe22488e34be2dbeee618b70fb
Andres <andresmunozgo@gmail.com>
2020-11-26 23:56:46 -0300

    se modifica archivo carta.rb

--- carta.rb
+++ carta.rb
@@ -4,6 +4,7 @@ class carta
     attr_accessor :numero, :pinta
     def initialize(numero, pinta)
         raise RangeError, "El número de la carta debe estar en un rango del 1 al 13" if numero < 1 || numero > 13
+        raise ArgumentError, "La pinta Corazón: C , Diamante: D, Espada: E o Trébol: T" if pinta !='C' || pinta !='D' || pinta !='E' || pinta !='T'
         @numero = numero
         @pinta = pinta
     end
